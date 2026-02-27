acd6ae9459f2180d2f3f4bd9ca6f34a1a3819be5
Maximilian Wörz <Maximilian-woerz12@gmx.de>
2026-02-27 16:24:01 +0100

    adding dialog, --> remove button in progress

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let basketBtndefault = "Add to basket"
 let basketBtnAdd = "Added"
+let basket = false;
+let priceDish = 0;
+let dishAmount = 0;
+let subTotalPrice = 0;
+let totalPrice = 0;
+const deliveryFee = 4.99;
 
 function loadPage() {
     loadHero();
     loadSections();
     loadDishes();
+    checkBasket();
 }
+
+/*start load function*/
 
 function loadHero() {
     const hero = document.getElementById('hero-section');
@@ -15,7 +24,7 @@ function loadHero() {
 }
 
 function loadSections() {
-    let main = document.getElementById('main');
+    let main = document.getElementById('main-content');
     for (let i = 0; i < myCategorys.length; i++) {
         main.innerHTML += templateSection(i);
     }
@@ -39,3 +48,153 @@ function loadDishes() {
         section.innerHTML += temp;
         }
     }
+
+    /*calculation*/
+
+function calculatePrice(amount, price) {
+    priceDish = amount * price;
+    subTotalPrice = 0;
+    totalPrice = 0;
+    for (const i in myDishes) {
+        subTotalPrice += myDishes[i].amount * myDishes[i].price;
+    }
+    totalPrice = subTotalPrice + deliveryFee;
+}
+
+    /*menu function*/
+
+function addDish(i) {
+    if (myDishes[i].amount < 10) {
+        let dishIdButton = document.getElementById('add-Basket'+ i)
+        myDishes[i].amount = myDishes[i].amount + 1;
+        calculatePrice(myDishes[i].amount, myDishes[i].price);
+        updateBasketItem(i);
+        dishIdButton.innerHTML = basketBtnAdd + " " + myDishes[i].amount;       
+    }
+    else {
+        openPopUpMaxAmount(i);
+    }
+}
+
+/*basket*/
+
+function checkBasket() {
+    let tempHtml = "";
+    for(const i in myDishes){
+        if (myDishes[i].amount  > 0) {
+            let tempName = myDishes[i].name;
+            let tempAmount = myDishes[i].amount;
+            let tempPrice = myDishes[i].price;
+            calculatePrice(tempAmount, tempPrice);
+            tempHtml += templateBasketItems(i, priceDish.toFixed(2));
+            basket = true;
+        }
+    }
+    if (basket === true) {
+        loadBasket(tempHtml);
+    }
+    else {
+        document.getElementById('basket').innerHTML = templateBasketEmpty();
+    }
+}
+
+function loadBasket(tempHtml) {
+    let basket = document.getElementById('basket');
+    basket.innerHTML = templateBasket(subTotalPrice.toFixed(2), totalPrice.toFixed(2) , deliveryFee);
+    let basketItem = document.getElementById('basket-items');
+    basketItem.innerHTML = tempHtml;
+}
+
+function emptyTheBasket() {
+    for (const i in myDishes) {
+        myDishes[i].amount = 0;
+        let btnMenu = 'add-Basket' + i;
+        document.getElementById(btnMenu).innerHTML = basketBtndefault;
+
+    }
+    basket = false;
+    document.getElementById('basket').innerHTML = templateBasketEmpty();
+}
+
+function basketChangeAmount() {
+    
+}
+
+
+function updateBasketItem(i) {
+    if (basket === true) {
+        if (myDishes[i].amount > 1) {
+            let basketItemName = document.getElementById('basket-dish-name' + i)
+            let basketItem = document.getElementById(myDishes[i].name + '-overview')
+            basketItem.innerHTML = templateUpdateBasketItem(i, priceDish.toFixed(2));
+            basketItemName.innerHTML = myDishes[i].amount + " x " + myDishes[i].name;
+            updateBasketItemAssets(i);
+        }
+        else {
+           let basketItem = document.getElementById('basket-items');
+           basketItem.innerHTML += templateBasketItems(i, priceDish.toFixed(2));
+        }
+        updateBasketValues();
+    }
+    else {
+        checkBasket();
+    }
+}
+
+function updateBasketItemAssets(i) {
+    let btnTop = document.getElementById(myDishes[i].name + '-trashBtn-top');
+    let btnBot = document.getElementById(myDishes[i].name + '-trashBtn-bottum');
+    if (myDishes[i].amount > 1) {
+        btnTop.innerHTML = templateTrashButton();
+        btnBot.innerHTML = "-";
+    }
+    else {
+        btnTop.innerHTML = "";
+        btnBot.innerHTML = templateTrashButton();
+    }
+
+}
+
+function updateBasketValues() {
+        let priceTotalBtn = document.getElementById('price-total-btn');
+        let priceTotal = document.getElementById('price-total');
+        let priceSubtotal = document.getElementById('price-subtotal');
+        priceSubtotal.innerHTML = priceDish.toFixed(2) + "€";
+        priceTotalBtn.innerHTML = "Buy now " + totalPrice.toFixed(2) + "€";
+        priceTotal.innerHTML = totalPrice.toFixed(2) + "€";
+}
+
+function deletItemBasket(id) {
+    let deleteItem = document.getElementById(myDishes[id].name);
+    deleteItem.innerHTML = "";
+}
+
+
+/*Dialog*/
+
+function openPopUpMaxAmount() {
+    dialogRef = document.getElementById('dialog');
+    dialogRef.innerHTML = templateDialogOrderDenied();
+    openDialog();
+}
+
+function openPopUpDeliverOrder() {
+    dialogRef = document.getElementById('dialog');
+    dialogRef.innerHTML = templateDialogOrderDeliver();
+    openDialog();
+    emptyTheBasket();
+}
+
+function openDialog() { 
+    const dialogRef = document.getElementById('dialog');
+    dialogRef.showModal();
+    dialogRef.classList.add('open');
+    document.body.classList.add('no-scroll');
+}
+
+function closeDialog() { 
+    const dialogRef = document.getElementById('dialog');
+    dialogRef.close();
+    dialogRef.classList.remove('open');
+    document.body.classList.remove('no-scroll');
+}
--- scripts/template.js
+++ scripts/template.js
@@ -7,9 +7,9 @@ function templateDishes(i, btn) {
                     <h3 class="menu-name font-s-24 m-0 font-w-600" id="menu1">${myDishes[i].name}</h3>
                     <span class=" font-s-16">${myDishes[i].description}</span>
                 </div>
-                <div class="add-item">
+                <div class="add-item" id="add-item${i}">
                     <span class="font-s-24 font-w-600">${myDishes[i].price}€</span>
-                    <button class="font-s-16 font-w-600 btn p-8" id="add-Basket1">${btn}</button>
+                    <button class="font-s-16 font-w-600 btn p-8" id="add-Basket${i}" onclick="addDish(${i})">${btn}</button>
                 </div>
             </div>
     `
@@ -41,13 +41,125 @@ function templateHero(rating, reviews) {
 
 function templateSection(i) {
     return `
-            <section class="menu-section"  id="menu-section${i}">
+            <div class="menu-section"  id="menu-section${i}">
                 <div class="div-line d-flex-center m-b80">
                     <div class="wrapper div-line-positioning">
                         <img src="${myCategorys[i].icon}" alt="Icon of ${myCategorys[i].name}">
                         <h2 class="clr-white font-s-48 m-0 pb-8">${myCategorys[i].name}</h2>
                     </div>
                 </div> 
-            </section>
+            </div>
+    `
+}
+
+
+
+function templateBasket(priceMeals, priceTotal, deliveryFee) {
+    return `
+            <div class="basket-container">
+                <h2 class="font-w-700 font-s-32 clr-white">Your Basket</h2>
+                <div class="scroll-container" id="basket-items"></div>
+                <div class="m-b24"> 
+                    <div class="font-w-600 font-s-20 clr-white pb-8 d-flex-space-btw">
+                        <span >Subtotal</span>
+                        <span id="price-subtotal">${priceMeals}€</span>
+                    </div>
+                    <div class="font-w-600 font-s-20 clr-white pb-8 d-flex-space-btw">
+                        <span>Delivery fee</span>
+                        <span>${deliveryFee}€</span>
+                    </div>
+                    <div class="div-line-black"></div>
+                    <div class="font-w-700 font-s-22 clr-white pt-8 d-flex-space-btw">
+                        <span>Total</span>
+                        <span id="price-total">${priceTotal}€</span>
+                    </div>
+                </div>
+                <button class="font-w-700 font-s-32 btn-buy clr-white clr-backg-orange" id="price-total-btn" onclick="openPopUpDeliverOrder()">Buy now ${priceTotal}€</button>
+            </div> 
+    `
+}
+
+function templateBasketEmpty() {
+    return `
+            <div class="basket-container">
+                <h2 class="font-w-700 font-s-32 clr-lightOrange">Your Basket</h2>
+                <div class="font-w-500 font-s-24 clr-lightOrange text-align-c m-b80">
+                    <span>Nothing here yet.</span>
+                    <br>
+                    <span>Go ahead and choose something delicious!</span>
+                </div>
+                <div class="d-flex-center">
+                    <img class="img-shop-card" src="./assets/icons/shopping_cart.png" alt="">
+                </div>
+            </div>
+    `
+}
+
+
+function templateBasketItems(i, priceMeal) {
+    return `
+                    <div class="clr-backg-white  basket-meal-container" id="${myDishes[i].name}">
+                        <div class="d-flex-space-btw">
+                            <span class="font-w-700 font-s-22 clr-gray" id="basket-dish-name${i}">${myDishes[i].amount} x ${myDishes[i].name}</span>
+                            <button class="btn-white" id="${myDishes[i].name}-trashBtn-top" onclick="deletItemBasket(${i})"></button>
+                        </div>
+                        <div class="d-flex-space-btw" id="${myDishes[i].name}-overview">
+                            <div class="d-flex-center">
+                                <button class="font-w-600 font-s-24 btn-white" id="${myDishes[i].name}-trashBtn-bottum" onclick="deletItemBasket(${i})">
+                                    <img src="./assets/icons/delete.png">
+                                </button>
+                                <div class="font-w-600 font-s-24 transformY-4">1</div>
+                                <button class="font-w-600 font-s-24 btn-white">+</button>
+                            </div>
+                            <span class="font-w-700 font-s-22 clr-gray">${priceMeal}€</span>
+                        </div>
+                    </div>
+    `
+}
+
+function templateUpdateBasketItem(i, priceMeal) {
+    return `
+            <div class="d-flex-center">
+                <button class="font-w-600 font-s-24 btn-white" id="${myDishes[i].name}-trashBtn-bottum" onclick="deletItemBasket(${i})">
+                    <img src="./assets/icons/delete.png">
+                </button>
+                <div class="font-w-600 font-s-24 transformY-4">${myDishes[i].amount}</div>
+                <button class="font-w-600 font-s-24 btn-white">+</button>
+            </div>
+            <span class="font-w-700 font-s-22 clr-gray">${priceMeal}€</span>
+    `
+}
+
+
+function templateDialogOrderDeliver() {
+    return `
+            <div class="clr-backg-black clr-white dialog-container p-32 border-r-12">
+                <div class="dialog-close">
+                    <button class="btn-white clr-backg-black clr-white font-s-24" onclick="closeDialog()"> x </button>
+                </div>
+                <img src="./assets/icons/foodsTruck.png" class="transform-rotate-180">
+                <span class="font-w-700 font-s-42">Order confirmed!</span>
+                <span class="font-w-700 font-s-29">Youre food is on the way!</span>
+            </div>
+    `
+}
+
+function templateDialogOrderDenied() {
+    return `
+            <div class="clr-backg-black clr-white dialog-container p-32 border-r-12">
+                <div class="dialog-close">
+                    <button class="btn-white clr-backg-black clr-white font-s-24" onclick="closeDialog()"> x </button>
+                </div>
+                <img src="./assets/icons/Logo-Bestell-App.png" class="m-b80">
+                <span class="font-w-700 font-s-42 m-b24">The order cannot be added!</span>
+                <span class="font-w-700 font-s-29">The maximum quantity is 10 per unit.</span>
+                <span class="font-w-700 font-s-29">We apologize for the inconvenience.</span>
+            </div>
+    `
+}
+
+function templateTrashButton() {
+    return `
+    <img src="./assets/icons/delete.png">
     `
 }
